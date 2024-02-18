eba716996bcde4d42ec0ecf7d31357594b072cf5
1AnanthK <52385310+1AnanthK@users.noreply.github.com>
2024-02-18 04:11:19 -0800

    Modifying UI for new/edit page

--- harvesthero/src/Restaurant.js
+++ harvesthero/src/Restaurant.js
@@ -1,33 +1,56 @@
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate } from 'react-router-dom';
 
-function Restaurant()
-{
-    const navigate = useNavigate(); // Initialize useNavigate
+function Restaurant() {
+    const navigate = useNavigate();
 
     const handleUserSelect = (userType) => {
-        switch(userType) {
-        /*case 'volunteer':
-            navigate('/donation-request');
-            break;
-        */
-        case 'edit':
-            navigate('/restaurantEdit');
-            break;
-        case 'make':
-            navigate('/donation-request');
-            break;
-        default:
-            console.log('Invalid user type:', userType);
+        switch (userType) {
+            /*case 'volunteer':
+                navigate('/donation-request');
+                break;
+            */
+            case 'edit':
+                navigate('/restaurantEdit');
+                break;
+            case 'make':
+                navigate('/donation-request');
+                break;
+            default:
+                console.log('Invalid user type:', userType);
         }
     };
 
-    return (   
-        <div>
-        <h2>Select what you would like to do</h2>
-        <button onClick={() => handleUserSelect('edit')}>Edit Current Forms</button>
-        <button onClick={() => handleUserSelect('make')}>Fill Out New Form</button>
+    return (
+        <div style={{ backgroundColor: 'lightgreen', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <h2 style={{ fontFamily: 'sans-serif', fontSize: '30px' }}>
+                Select Your Option
+            </h2>
+            <button
+                onClick={() => handleUserSelect('edit')}
+                style={{
+                    backgroundColor: 'blue',
+                    color: 'white',
+                    border: 'none',
+                    padding: '10px 20px',
+                    marginBottom: '10px', // Add margin at the bottom
+                    cursor: 'pointer',
+                }}
+            >
+                Edit Current Forms
+            </button>
+            <button
+                onClick={() => handleUserSelect('make')}
+                style={{
+                    backgroundColor: 'red',
+                    color: 'white',
+                    border: 'none',
+                    padding: '10px 20px',
+                    cursor: 'pointer',
+                }}
+            >
+                Fill Out New Form
+            </button>
         </div>
-
     );
 }
 
